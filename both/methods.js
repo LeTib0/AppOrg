2e8e982133af143b8c83655112c09828290fe5ad
LeTib0 <LeTib0@users.noreply.github.com>
2019-10-21 08:36:56 +0400

    Quick Task ok

--- both/methods.js
+++ both/methods.js
@@ -1,6 +1,34 @@
 import {Categories,Tasks} from './collections'
 import { check } from 'meteor/check'
 Meteor.methods({
+
+    addQuickTask(infos){
+        check(infos,{
+            quickTaskTitle: String,  
+            quikTaskCategori: String
+        })
+        if(!this.userId) {
+            throw new Meteor.Error('not-connected', 'Veuillez d\'abord vous connecté')
+        }
+        let infoTask = {
+            quickTaskTitle: infos.quickTaskTitle,
+            quikTaskCategori: infos.quikTaskCategori,
+            createdAt: new Date(),
+            ownerId: this.userId,
+            ownerPseudo:Meteor.user().username
+        }
+        let categori= {
+            name: infos.quikTaskCategori,
+            createdAt: new Date(),
+            createBy: Meteor.user().username
+        }
+        //Penser à vérifier si la catégorie existe
+        
+        Tasks.insert(infoTask)
+        Categories.insert(categori)
+    },
+    
+
     addTasks(information) {
         check(information, {
             taskTitle: String,  
